fe53bdf1766de0c329b8a94012ed392fdac30844
HansBlackCat <jeremy3599@snu.ac.kr>
2020-01-29 22:28:20 +0900

    c++

--- Cpplang/casting_and_nth_array.cpp
+++ Cpplang/casting_and_nth_array.cpp
@@ -18,9 +18,37 @@ class Array {
             for (int i = 0; i < dim; i++) size[i] = array_size[i];
         }
         
+        //
+        void initialize_address(Address* current) {
+            if (!current) return;
+            if(current -> level == dim - 1) {
+                current -> next = new int[size[current -> level]];
+                return;
+            }
+            current -> next = new Address[size[current -> level]];
+            for (int i = 0; i != size[current -> level]; i++) {
+                (static_cast<Address*>(current -> next) + i) -> level = current -> level + 1;
+                initialize_address(static_cast<Address*>(current -> next) + i);
+            }
+        }
+        void delete_address(Address* current) {
+            if (!current) return;
+            for (int i = 0; current -> level < dim - 1 && i < size[current -> level]; i++) {
+                delete_address(static_cast<Address*>(current -> next) + i);
+            }
+            delete[] static_cast<Address*>(current -> next);
+        }
+        ~Array() {
+            delete_address(top);
+            delete[] size;
+        }
+
+        /*
         void initialize_address(Address* current);
         void delete_address(Address* current);
+        */
 };
+/*
 void Array::initialize_address(Array::Address* current) {
     if (!current) return;
     if (current -> level == dim - 1) {
@@ -33,12 +61,13 @@ void Array::initialize_address(Array::Address* current) {
         initialize_address(static_cast<Address*>(current -> next) + i);
     }
 }
-/*
+
 void Array::delete_address(Array::Address* current) {
     if(!current) return;
     for (int i = 0; current -> level < dim - 1 && i < size[current -> level]; i++) {
         delete_address(static_cast<Address*>(current -> next) + i);
     }
+    delete[] current -> next;
 }*/
 
 
@@ -54,4 +83,5 @@ int main() {
     arr = new int*[x1];
     for (int i = 0; i < x1; i++) arr[i] = new int[x2];
 
+    std::cout<<"END_PROCESS"<<std::endl;
 }
--- Cpplang/std_string_and_inherit.cpp
+++ Cpplang/std_string_and_inherit.cpp
@@ -3,6 +3,61 @@
 
 class Employee {
     std::string name;
+    int age;
+
+    std::string position;
+    int rank;
+
+    public:  
+        // constructor
+        Employee(std::string name, int age, std::string position, int rank): name(name), age(age), position(position), rank(rank) {}
+        // copy constructor
+        Employee(const Employee& employee) {
+            name = employee.name;
+            age = employee.age;
+            position = employee.position;
+            rank = employee.rank;
+        }
+        // default constructor
+        Employee() {}
+
+        int calculate_pay() {return 200 + rank * 50;}
+        void print_info() {
+            std::cout << name << " (" << position << " , " << age << ") ==> " 
+            << calculate_pay() << "$" << std::endl;
+        }
+};
+
+class EmployeeList {
+    int alloc_employee;
+    int current_employee;
+    Employee** employee_list;
+
+    public:  
+        EmployeeList(int alloc_employee): alloc_employee(alloc_employee) {
+            employee_list = new Employee*[alloc_employee];
+            current_employee = 0;
+        }
+        void add_employee(Employee* employee) {
+            employee_list[current_employee] = employee;
+            current_employee++;
+        }
+        int current_employee_num() {return current_employee;}
+
+        void print_employee_info() {
+            int total_pay = 0;
+            for (int i = 0; i < current_employee; i++) {
+                employee_list[i] -> print_info();
+                total_pay += employee_list[i] -> calculate_pay();
+            }
+            std::cout << "Total cost : " << total_pay << "$" << std::endl;
+        }
+        ~EmployeeList() {
+            for (int i = 0; i < current_employee; i++) {
+                delete employee_list[i];
+            }
+            delete[] employee_list;
+        }
 };
 
 int main() {
@@ -12,7 +67,9 @@ int main() {
     std::cout << s.length() << std::endl;
     std::cout << s.max_size() << std::endl;
     std::cout << s+s << std::endl;
-    
-    
+    std::cout << std::endl;
+
+    EmployeeList emp_list(10);
+
     return 0;
 }
